ae1f653f8ef30ddafc8bb50a2aa951df6816adb2
VigroX <VigroX@users.noreply.github.com>
2019-01-10 20:32:47 +0200

    0.0.8.17
    Small bug fixes and improvements.
    * Fix decide.js

--- commands/decide.js
+++ commands/decide.js
@@ -3,15 +3,13 @@ exports.run = async (client, message, args) => {
 		return message.reply(":no_entry_sign: This module is disabled!");
 	}
 	if(!args[1]) return message.reply("There must be atleast two words to decide from!");
-	let wordArray = message.content.split(" ");
+	let wordArray = arguments.split(" ");
 	let result = wordArray[Math.floor(Math.random() * wordArray.length)];
 	let arguments = args.join(", ");
 	let embed = new client.discord.RichEmbed()
 	.setAuthor(message.author.tag)
 	.setColor("BLUE")
 	.addField("Decided from:", arguments)
-	.addField("DEBUG: ARGS", args.content)
-	.addField("DEBUG: MESSAGES", message.content)
 	.addField("Result", result);
 	message.channel.send(embed);
 };
