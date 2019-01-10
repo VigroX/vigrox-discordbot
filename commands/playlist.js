d28db97e3c1813e76f2b8981fb50a8a1c563b95e
VigroX <VigroX@users.noreply.github.com>
2019-01-10 20:23:02 +0200

    0.0.8.16
    Small bug fixes and improvements.
    * Fix decide.js
    * Fix playlist.js

--- commands/playlist.js
+++ commands/playlist.js
@@ -9,10 +9,10 @@ exports.run = async (client, message, args) => {
 	.setColor("BLUE")
 	.setTitle("Playlist [INFO]")
 	.setDescription(`TIP: ${client.config.prefix}yt playlist`)
-	.addField("1. YouTube URL", "${client.config.prefix}play https://www.youtube.com/playlist?list=" + process.env.YOUTUBE_PLAYLIST)
-	.addField("2. Randomize Queue (optional)", "${client.config.prefix}yt shuffle")
-	.addField("3. Repeat Playlist (optional)", "${client.config.prefix}yt loop")
-	.addField("4. Skip Current (optional)", "${client.config.prefix}yt skip")
+	.addField("1. YouTube URL", `${client.config.prefix}play https://www.youtube.com/playlist?list=` + process.env.YOUTUBE_PLAYLIST)
+	.addField("2. Randomize Queue (optional)", `${client.config.prefix}yt shuffle`)
+	.addField("3. Repeat Playlist (optional)", `${client.config.prefix}yt loop`)
+	.addField("4. Skip Current (optional)", `${client.config.prefix}yt skip`)
 	.setTimestamp()
 	.setFooter(`${client.user.username}`, `${client.user.avatarURL}`);
 	return message.channel.send(embed);
